f568135252239900ca75b4f51620cb59f6350c5b
Boleklolo <mateuszmaz1986@protonmail.com>
2023-07-17 23:51:35 +0200

    Mario lol

--- MarioJingle.cs
+++ MarioJingle.cs
@@ -8,25 +8,25 @@ namespace MyProgram
         static void Main()
         {
            {
-        int duration = 200; // Duration of notes and rests in milliseconds
+            int duration = 200; // Duration of notes and rests in milliseconds
 
-        // Frequencies for the notes (in hertz)
-        int e = 330;
-        int eLow = 165;
-        int c = 261;
-        int d = 294;
-        int g = 392;
-        int G = 196;
-        int a = 440;
-        int A = 220;
-        int b = 494;
-        int f = 349;
-        int F = 175;
-        int E = 659;
+            // Frequencies for the notes (in hertz)
+            int e = 330;
+            int eLow = 165;
+            int c = 261;
+            int d = 294;
+            int g = 392;
+            int G = 196;
+            int a = 440;
+            int A = 220;
+            int b = 494;
+            int f = 349;
+            int F = 175;
+            int E = 659;
 
 
 
-       int[] melody = {e, e, 0, e, 0, c, e, 0,
+            int[] melody = {e, e, 0, e, 0, c, e, 0,
                         g, 0, 0, 0, G, 0, 0, 0,
                         c, 0, 0, g, 0, 0, e, 0,
                         0, a, 0, b, 0, A, A, 0,
@@ -34,19 +34,19 @@ namespace MyProgram
                         a, 0, f, g, 0, e, 0, c,
                         d, b, 0, 0, G, F, E, 0, 0, eLow, 0, 0};
 
-        // Play the melody
- foreach (int note in melody)
-        {
-            if (note != 0)
+            // Play the melody
+            foreach (int note in melody)
             {
-                Console.Beep(note, duration);
+                if (note != 0)
+                {
+                    Console.Beep(note, duration);
+                }
+                else
+                {
+                    Thread.Sleep(duration);
+                }
             }
-            else
-            {
-                Thread.Sleep(duration);
             }
-    }
         }
     }
-    }
-    }
+}
